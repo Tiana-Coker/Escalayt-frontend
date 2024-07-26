568b8efecaf605b9e1a1ca6b244de2a431ef5b0a
Chinecherem <chinecheremubawike@gmail.com>
2024-07-26 12:17:27 +0100

    More boilerplate changes

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import react from "./react.svg";
+import PROFILE from "./profile.svg"
+//
+//
+//
+
+
+export default {react, PROFILE}
---
+++ src/components/modals/createTicket/CreateTicket.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from "./CreateTicket.module.css";
+
+export default function CreateTicket() {
+  return (
+    <div>CreateTicket</div>
+  )
+}
