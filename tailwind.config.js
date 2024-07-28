6cde6be9061f765217d09bf3edb88a759d7d525d
Maxiflexy <onyemax247@gmail.com>
2024-07-28 09:11:15 +0100

    create new ticket and new category UI and logic for interacting with backend API

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 8px 8px -4px #1018280A, 0px 20px 24px -4px #1018281A",
+      },
+      filter: {
+        blurred: "blur(5px)",
+      },
+    },
   },
   plugins: [],
-}
+};
