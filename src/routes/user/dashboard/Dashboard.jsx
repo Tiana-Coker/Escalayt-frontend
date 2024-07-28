54d2818cbe9f87e0fdbb4fcb7add1976940de4e2
Maxiflexy <onyemax247@gmail.com>
2024-07-28 10:16:19 +0100

    create new ticket and new category UI and logic for interacting with backend API

--- src/routes/user/dashboard/Dashboard.jsx
+++ src/routes/user/dashboard/Dashboard.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import CreateTicket from "../../../components/modals/createTicket/CreateTicket";
 
 export default function Dashboard() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  
   return (
-    <div>Dashboard</div>
+    <>
+      <div className={`relative ${isModalOpen ? "filter-blurred" : ""}`}>
+      <p>Hello, this is the User Dashboard </p>
+        <button
+          onClick={handleOpenModal}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Create Ticket
+        </button>
+        {isModalOpen && <CreateTicket onClose={handleCloseModal} />}
+      </div>
+      {/* <div className={`relative ${isModalOpen ? "filter-blurred" : ""}`}>
+        <p>.</p>
+        <br />
+        <button
+          onClick={handleOpenModal2}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Create Category
+        </button>
+        {isModalOpen2 && <CreateCategory onClose={handleCloseModal2} />}
+
+        <p>Hello, this is the Escalayt Dashboard </p>
+      </div> */}
+    </>
   )
 }
