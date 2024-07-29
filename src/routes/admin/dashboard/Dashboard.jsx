ad5695d88cc9a45acbbbbc1632e17f39b8d788a9
samuelgbenga <samuelgbenga972@gmail.com>
2024-07-29 18:11:35 +0100

    department design flow

--- src/routes/admin/dashboard/Dashboard.jsx
+++ src/routes/admin/dashboard/Dashboard.jsx
@@ -2,11 +2,16 @@
 import React, { useState } from "react";
 import CreateTicket from "../../../components/modals/createTicket/CreateTicket";
 import CreateCategory from "../../../components/modals/createCategory/CreateTicketCategory";
+import CreateDepartment from "../../../components/modals/createDepartment/CreateDepartment";
+
 
 export default function Dashboard() {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalOpen2, setIsModalOpen2] = useState(false);
+
+   // handle create department state modal
+   const [isModalOpen3, setIsModalOpen3] = useState(false);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -15,11 +20,19 @@ export default function Dashboard() {
     setIsModalOpen2(true);
   };
 
+  const handleOpenModal3 = () => {
+    setIsModalOpen3(true);
+  };
+
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
   const handleCloseModal2 = () => {
     setIsModalOpen2(false);
+  };
+
+  const handleCloseModal3 = () => {
+    setIsModalOpen3(false);
   };
 
   return (
@@ -45,6 +58,20 @@ export default function Dashboard() {
         </button>
         {isModalOpen2 && <CreateCategory onClose={handleCloseModal2} />}
       </div>
+
+       {/* handle create department modal */}
+       <div className={`relative ${isModalOpen3 ? "filter-blurred" : ""}`}>
+        <p>.</p>
+        <br />
+        <button
+          onClick={handleOpenModal3}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Create Department
+        </button>
+        {isModalOpen3 && <CreateDepartment onClose={handleCloseModal3} />}
+      </div>
+
     </>
   );
 }
