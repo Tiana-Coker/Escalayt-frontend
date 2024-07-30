b836aa7a2f797b3853a25fbb723e6123f904a7b0
abayomiogunlowo <abayomiogunlowo@gmail.com>
2024-07-30 21:22:43 +0100

    ZAP!
    Added all components and made the page mobile responsive.

---
+++ src/components/landingPage/Content1.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import TwoWorkers from '../../assets/landingPage/workers.png'
+import check from '../../assets/landingPage/icon.svg'
+import arrowRight from '../../assets/landingPage/arrow-right.svg'
+
+function Content1() {
+  return (
+    <>
+    <div className='container mx-auto px-4 flex flex-col md:flex-row items-center justify-between'> 
+    <br />
+    <br />
+      <div>
+        <img src={TwoWorkers} alt="Two workers pointing at something"  className="rounded-lg"/>
+      </div>
+        <div>
+        <br />
+        <br />
+            <h3 className='text-4xl font-bold mb-6'>Key Features</h3>
+            <p className='text-lg mb-4'>Discover the powerful features of Escalayt that<br/> make facility management a breeze</p>
+            <ul>
+                <li className='flex items-center'><img src={check} alt="check" className='mr-2' />Easy Ticket Creation</li>
+                <li className='flex items-center'><img src={check} alt="check" className='mr-2'/>Real-Time Tracking </li>
+                <li className='flex items-center'><img src={check} alt="check" className='mr-2'/>Detailed Reports and Analytics</li>
+                <li className='flex items-center'><img src={check} alt="check" className='mr-2'/>Team Collaboration</li>
+                <li className='flex items-center'><img src={check} alt="check" className='mr-2'/>User-Friendly Interface</li>
+            </ul>
+            <div className='mr-6 font-bold p-6'>
+            <button className="flex items-center font-bold">
+              Make your first sale
+              <img src={arrowRight} alt="An arrow" className="ml-2" />
+           </button>
+            </div>
+            <br />
+            <br />
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Content1
--- src/routes/landingPage/LandingPage.jsx
+++ src/routes/landingPage/LandingPage.jsx
@@ -1,8 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Header from '../../components/landingPage/Header'
+import Content from '../../components/landingPage/Content'
+import Content1 from '../../components/landingPage/Content1'
+import Content2 from '../../components/landingPage/Content2'
+import Content3 from '../../components/landingPage/Content3'
+import Testimonials from '../../components/landingPage/Testimonials'
+import Footer from '../../components/landingPage/Footer'
 
-export default function LandingPage() {
+function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div>
+        <Header />
+        <Content />
+        <Content1 />
+        <Content2 />
+        <Content3 />
+        <Testimonials />
+        <Footer />
+    </div>
   )
 }
+
+export default LandingPage
